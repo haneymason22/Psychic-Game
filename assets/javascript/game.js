f5e962b74de2488afc69abf7c33befc105f66ee0
MHane952 <haneymason22@gmail.com>
2019-10-25 11:16:15 -0500

    text ids and javascript added

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,1 +1,14 @@
-<
+var computerChoices = [a, b, c, d, e, f, g, h, i, j, k, l, m, n, o, p, q, r, s, t, u, v, w, x, y, z];
+var userChoiceText = document.getElementById("userchoice-text");
+var computerChoiceText = document.getElementById("computerchoice-text");
+
+document.onkeyup = function(event) {
+    var userGuess = event.key;
+    var computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
+    var guessCounter = 9;
+    for (var j =0; j < computerChoices.length; j ++;) {
+        if (userGuess === computerGuess) {
+            wins ++;
+        }; 
+    };
+};
